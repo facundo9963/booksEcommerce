dbed24ffc1f35febf2fe106a84129be46532e199
Facundo Sánchez <facundosanchez9963@gmail.com>
2022-05-31 14:15:10 -0300

    Se agregó la función de login y se utilizó bcrypt para hashear la contraseña para mas seguridad

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express")
-const {createUser} = require("../controllers/userController")
+const {createUser, login} = require("../controllers/userController")
 const router = Router()
 
 router.post("/register", createUser)
+router.post("/login", login)
 
 
 module.exports = router
